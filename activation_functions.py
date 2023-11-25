c4640ff2728ee1dbb76b0e5ade3f6d3e09e52b43
Jesse Alves <jessalves2@gmail.com>
2023-11-25 15:27:39 +0100

    first commit

---
+++ activation_functions.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Main activation functions used in neural networks
+
+def stepFunction(sum):
+    if (sum >= 0):
+        return 1
+    return 0
+
+def sigmoidFunction(sum):
+    return 1 / (1 + np.exp(-sum))
+
+def tanhFunction(sum):
+    return (np.exp(sum) - np.exp(-sum))/(np.exp(sum) + np.exp(-sum))
+
+
+print(stepFunction(5))
+print(sigmoidFunction(5))
+print(tanhFunction(5))
+
