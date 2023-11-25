37b04d9fe95f1f282061ef816d89a73ed8ac5551
Jesse Alves <jessalves2@gmail.com>
2023-11-25 16:02:52 +0100

    update

--- activation_functions.py
+++ activation_functions.py
@@ -2,16 +2,29 @@ import numpy as np
 
 # Main activation functions used in neural networks
 
-def stepFunction(sum):
+def stepFunction(sum): #Problems that can be linearly separate
     if (sum >= 0):
         return 1
     return 0
 
-def sigmoidFunction(sum):
+def sigmoidFunction(sum): #Problems with binary classification
     return 1 / (1 + np.exp(-sum))
 
-def tanhFunction(sum):
+def tanhFunction(sum): # Problems betwenn -1 and 1
     return (np.exp(sum) - np.exp(-sum))/(np.exp(sum) + np.exp(-sum))
+
+# High used function
+def ReLU(sum): # used in CNN
+    if (sum >=0):
+        return sum
+    return 0
+
+def linearFunction(sum): # used in regression
+    return sum
+
+def softmaxFunction(x): # used in multi-classification
+    ex = np.exp(x)
+    return ex  /ex.sum()
 
 
 print("Testing functions:")
@@ -19,3 +32,5 @@ print(" ")
 print(stepFunction(5))
 print(sigmoidFunction(5))
 print(tanhFunction(5))
+print(ReLU(5))
+print(softmaxFunction(np.array([5,6,7,8,9])))
